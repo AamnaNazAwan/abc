17a3b0a5437a617ea96aad90d3f5c976f3370582
AAMNA NAZ AWAN <aamnanaz403@gmail.com>
2021-10-29 22:11:31 -0700

    class 3

--- src/com/aamnanazawan/ned.java
+++ src/com/aamnanazawan/ned.java
@@ -1,5 +1,27 @@
 package com.aamnanazawan;
 
 public class ned {
+    class shape {
+        private int height;
+        private int width;
 
+        public int getHeight() {
+            return height;
+        }
+
+        public void setHeight(int height) {
+            this.height = height;
+        }
+
+        public int getWidth() {
+            return width;
+        }
+
+        public void setWidth(int width) {
+            this.width = width;
+        }
+
+        shape s = new shape();
+
+    }
 }
